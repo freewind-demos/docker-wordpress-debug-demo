1371d8fa74bf2d12b4a887ee51aa2d479cbd6812
Freewind <nowindlee@gmail.com>
2020-01-31 19:03:23 +0800

    write_log

--- WordPress-5.3.2/test.php
+++ WordPress-5.3.2/test.php
@@ -1,13 +1,31 @@
 <?php
-define( 'WP_DEBUG', true );
-define( 'WP_DEBUG_LOG', true );
-define( 'WP_DEBUG_DISPLAY', true );
-define( 'SCRIPT_DEBUG', true );
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', true);
+define('WP_DEBUG_DISPLAY', true);
+define('SCRIPT_DEBUG', true);
 ?>
 
 <?php
-require( dirname( __FILE__ ) . '/wp-load.php' );
+if (!function_exists('write_log')) {
+    function write_log($log)
+    {
+        if (WP_DEBUG === true) {
+            if (is_array($log) || is_object($log)) {
+                error_log(print_r($log, true));
+            } else {
+                error_log($log);
+            }
+        }
+    }
+}
+?>
 
-error_log('my-error_log');
+
+<?php
+require(dirname(__FILE__) . '/wp-load.php');
+
+error_log('my error_log');
+write_log('my write_log');
+
 throw new Error('Hello-error')
 ?>
